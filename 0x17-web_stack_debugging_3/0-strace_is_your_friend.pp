d87960dd8723b479831bee3d7906181301e5f4a7
Alouie412 <alouie412@gmail.com>
2020-05-20 19:03:34 +0000

    puppet-lint compliance

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # This Puppet script fixes a typo that causes error 500 on Wordpress. ...Seriously?
 exec { 'replace':
-  command   => "sed -i -e 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  path      =>  '/bin',
+  command => "sed -i -e 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  path    =>  '/bin',
 }
